88ec4762124386791964716f59b37fc18a1be9c6
sotzee <sotzee@gmail.com>
2026-08-14 16:50:47 -0700

    run MassRadius of selection and test seperately.

---
+++ run_MRselection_batch.bash
@@ -0,0 +1,3 @@
+for i in {0..1}; do
+    python main_MR_test.py data_model/model_batch${i} ./data_skyrme/
+done
--- run_MRtest_batch.bash
+++ run_MRtest_batch.bash
@@ -1,3 +1,3 @@
 for i in {0..1}; do
-    python main_MR_test.py data_model/model_batch${i} ./data_skyrme/
+    python main_MR_test_finalTest.py data_model/model_batch${i} ./data_skyrme/
 done
